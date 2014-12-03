d78621baec007563027b3d2346a4c11177b5bdd4
mezm <bartleby.sev@gmail.com>
2014-12-03 14:52:55 +0200

    added embedded resource example

--- Mezm.Owin.Razor.Example/Startup.cs
+++ Mezm.Owin.Razor.Example/Startup.cs
@@ -4,20 +4,28 @@ using Owin;
 
 namespace Mezm.Owin.Razor.Example
 {
+    using Microsoft.Owin.FileSystems;
+
     public class Startup
     {
         public void Configuration(IAppBuilder appBuilder)
         {
             appBuilder.UseErrorPage();
-            appBuilder.UseRazor(InitRoutes);
+            appBuilder.UseRazor(InitFileViewRoutes);
+            appBuilder.UseRazor(InitEmbeddedViewRoutes, new EmbeddedResourceFileSystem());
         }
 
-        private static void InitRoutes(IRouteTable table)
+        private static void InitFileViewRoutes(IRouteTable table)
         {
             table
                 .AddFileRoute("/", "Views/index.cshtml")
                 .AddFileRoute("/about/me", "Views/about.cshtml", new AboutMeModel { Name = "Val" })
                 .AddFileRoute("/a", "Views/with_layout.cshtml");
+        }
+
+        private static void InitEmbeddedViewRoutes(IRouteTable table)
+        {
+            table.AddFileRoute("/embedded", "/Mezm.Owin.Razor.Example.EmbeddedViews.index.cshtml");
         }
     }
 }
